3174660595b390b54c1b018e732cf0effa3be7e1
alabri0803 <rashid92759346@gmail.com>
2025-08-28 20:09:24 +0000

    Static

--- rent_management/settings.py
+++ rent_management/settings.py
@@ -125,6 +125,9 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
