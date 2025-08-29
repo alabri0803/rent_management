b6bc00da3b53568e3197e7f5cec0f54320791a61
alabri0803 <rashid92759346@gmail.com>
2025-08-29 09:31:40 +0000

    إنشاء portal/views

--- portal/views.py
+++ portal/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from dashboard.models import Lease, Tenant
 
-# Create your views here.
+class PortalDashboardView(LoginRequiredMixin, TemplateView):
+  template_name = 'portal/dashboard.html'
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    try:
+      tenant = Tenant.objects.get(user=self.request.user)
+      leases = Lease.objects.filter(tenant=tenant, status__in=['active', 'expired_soon']).first()
+      context['leases'] = leases
+      if leases:
+          context['payments'] = leases.payments.all()
+    except Tenant.DoesNotExist:
+       context['error'] = 'لا يوجد ملف مستأجر مرتبط بحسابك'
+    return context
