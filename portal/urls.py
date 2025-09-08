2a9ad219b9ae6a57f8126c0cb5f435de437c2fb9
alabri0803 <rashid92759346@gmail.com>
2025-09-08 13:38:51 +0000

    تحديث portal/urls.py

--- portal/urls.py
+++ portal/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import PortalDashboardView
+from .views import PortalDashboardView, MaintenanceRequestListView, MaintenanceRequestCreateView
 
 urlpatterns = [
   path('', PortalDashboardView.as_view(), name='portal_dashboard'),
+  path('maintenance/', MaintenanceRequestListView.as_view(), name='maintenance_request_list'),
+  path('maintenance/new/', MaintenanceRequestCreateView.as_view(), name='maintenance_request_create'),
 ]
