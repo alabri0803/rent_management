68e6d490a8b65f2d1f8e8b24806fefd54f35f5a0
alabri0803 <rashid92759346@gmail.com>
2025-09-09 10:39:21 +0000

    تحديث dashboard/forms.py

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Lease, Unit, MaintenanceRequest
+from .models import Lease, Unit, MaintenanceRequest, Document
 
 class LeaseForm(forms.ModelForm):
   class Meta:
@@ -40,3 +40,12 @@ class MaintenanceRequestUpdateForm(forms.ModelForm):
     super().__init__(*args, **kwargs)
     for field_name, field in self.fields.items():
       field.widget.attrs.update({'class':'w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-[#993333]'})
+
+class DocumentForm(forms.ModelForm):
+  class Meta:
+    model = Document
+    fields = ['title', 'file']
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.fields['file'].widget.attrs.update({'class':'w-full text-sm text-gray-500 file:px-4 file:mr-4 file:py-2 file:border-0 file:text-sm file:font-semibold file:bg-bule-50 file:text-bule-700 hover:file:bg-bule-100'})
