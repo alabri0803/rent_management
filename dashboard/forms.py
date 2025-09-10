1016cbf595ba65dbad351b334aaebc4dc7332af7
alabri0803 <rashid92759346@gmail.com>
2025-09-10 12:11:38 +0000

    تحديث dashboadr/forms.py

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Lease, Unit, MaintenanceRequest, Document
+from .models import Lease, Unit, MaintenanceRequest, Document, Expense
 
 class LeaseForm(forms.ModelForm):
   class Meta:
@@ -49,3 +49,19 @@ class DocumentForm(forms.ModelForm):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     self.fields['file'].widget.attrs.update({'class':'w-full text-sm text-gray-500 file:px-4 file:mr-4 file:py-2 file:border-0 file:text-sm file:font-semibold file:bg-bule-50 file:text-bule-700 hover:file:bg-bule-100'})
+
+class ExpenseForm(forms.ModelForm):
+  class Meta:
+    model = Expense
+    fields = ['building', 'category', 'description', 'amount', 'expense_date', 'receipt']
+    widgets = {
+      'expense_date': forms.DateInput(attrs={'type': 'date'}),
+    }
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    for field_name, field in self.fields.items():
+      if field_name != 'receipt':
+        field.widget.attrs.update({'class':'w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-[#993333]'})
+      else:
+        field.widget.attrs.update({'class':'w-full text-sm text-gray-500 file:px-4 file:mr-4 file:py-2 file:border-0 file:text-sm file:font-semibold file:bg-bule-50 file:text-bule-700 hover:file:bg-bule-100'})
