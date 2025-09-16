32b50e02051f4e773c5480fc444d4f0e718e0a4c
alabri0803 <rashid92759346@gmail.com>
2025-09-16 11:37:50 +0000

    I18n

--- rent_management/urls.py
+++ rent_management/urls.py
@@ -18,12 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('', include('portal.urls')),
+]
+urlpatterns += [
+    path('i18n/', include('django.conf.urls.i18n'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
