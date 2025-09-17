a504c889818b51630d897489468e84af93740935
alabri0803 <rashid92759346@gmail.com>
2025-09-17 11:36:18 +0000

    تحديث urls.pt

--- rent_management/urls.py
+++ rent_management/urls.py
@@ -22,13 +22,13 @@ from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
-    path('admin/', admin.site.urls),
 ]
 urlpatterns += i18n_patterns(
+    path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('portal/', include('portal.urls')),
-    prefix_default_language=True,
+    prefix_default_language=False,
 )
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
