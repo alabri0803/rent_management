64d3eba9479b498224c0f4996f8a44a2425db93d
alabri0803 <rashid92759346@gmail.com>
2025-09-22 16:58:49 +0000

    asd

--- rent_management/settings.py
+++ rent_management/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rm=85vc+pgs=^4+m+=d*32)+j7xx*_5t%amcq(=d7iz5q8zp^e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
