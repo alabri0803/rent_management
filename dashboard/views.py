f78fd92d6e82076025d4b14b4373291d6944f8b8
alabri0803 <rashid92759346@gmail.com>
2025-09-24 07:24:21 +0000

    le

--- dashboard/views.py
+++ dashboard/views.py
@@ -22,13 +22,13 @@ from django.template.loader import render_to_string
 
 def export_to_pdf(request):
     lease = get_object_or_404(Lease, pk=1)
-    payment = get_object_or_404(Payment, pk=1)
+    payment = get_object_or_404(Payment, pk=1, lease=lease)
     
     from num2words import num2words
     amount_in_words = num2words(payment.amount, lang='ar')
     # بيانات المثال
     context = {
-        'lease': lease,
+        'lease': payment.lease,
         'payment': payment,
         'title': 'تقرير باللغة العربية',
         'content': 'هذا محتوى عربي يدعم التشكيل والخطوط العربية',
