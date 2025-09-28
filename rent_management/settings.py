9973c91fc9b78b5a32bb0599302e553b99d4151b
alabri0803 <rashid92759346@gmail.com>
2025-09-29 01:29:29 +0400

    Update settings.py

--- rent_management/settings.py
+++ rent_management/settings.py
@@ -89,17 +89,27 @@ WSGI_APPLICATION = 'rent_management.wsgi.application'
         #'NAME': BASE_DIR / 'db.sqlite3',
     #}
 #}
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'q51hnrv8xdz5hf5x',
+        #'USER': 'letzn5s3o5vqlvte',
+        #'PASSWORD': 'rme7ieccesl6q4ck',
+        #'HOST': 'b8rg15mwxwynuk9q.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+        #'PORT': '3306',
+        #'OPTIONS': {
+            #'ssl': {'ssl-mode': 'require'}
+        #}
+    #}
+#}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'q51hnrv8xdz5hf5x',
-        'USER': 'letzn5s3o5vqlvte',
-        'PASSWORD': 'rme7ieccesl6q4ck',
-        'HOST': 'b8rg15mwxwynuk9q.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+        'NAME': 'rent_management',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
         'PORT': '3306',
-        'OPTIONS': {
-            'ssl': {'ssl-mode': 'require'}
-        }
     }
 }
 
